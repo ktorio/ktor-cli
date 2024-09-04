dcc12c97f43f12d0cea1e746c8bf92e1504c999e
Aleksei Tirman <aleksei.tirman@jetbrains.com>
2024-09-04 17:46:22 +0400

    Add PowerShell script for packing an msi installer (#25)

---
+++ packInstaller.ps1
@@ -0,0 +1,45 @@
+param (
+    [string]$toolPath = ".\build\windows\amd64\ktor.exe",
+    [string]$outPath = "ktor-installer.msi",
+    [string]$wixExe = "wix.exe"
+)
+
+$version = $(git describe --tags --contains --always --abbrev=7)
+
+if (!($version -match '\d+\.\d+.\d+')) {
+    Write-Error "Expected version in the format *.*.*, got ${version}"
+    exit 1
+}
+
+$wixProduct = @"
+<Wix xmlns="http://wixtoolset.org/schemas/v4/wxs" xmlns:ui="http://wixtoolset.org/schemas/v4/wxs/ui">
+    <Package Name="Ktor CLI" Version="${version}" Manufacturer="JetBrains" UpgradeCode="$(New-Guid)" Scope='perUser'>
+        <MediaTemplate EmbedCab="yes" />
+        <WixVariable Id="WixUILicenseRtf" Value="LICENSE.rtf" />
+
+        <MajorUpgrade
+                AllowDowngrades="no" DowngradeErrorMessage="A newer version of Ktor CLI is already installed"
+                AllowSameVersionUpgrades="yes"
+        />
+
+        <Property Id="WIXUI_EXITDIALOGOPTIONALTEXT" Value="Ktor CLI has been successfully installed. Use the ktor.exe alias on the command line to launch the tool." />
+
+        <StandardDirectory Id="LocalAppDataFolder">
+            <Directory Id="JetBrains" Name="JetBrains">
+                <Directory Id="INSTALLDIR" Name="KtorCLI">
+                    <Component Id="MainExecutable" Guid="$(New-Guid)">
+                        <Environment Id="PATH" Name="PATH" Value="[INSTALLDIR]" Permanent="yes" Part="last" Action="set" System="no" />
+                        <File Id="KtorExe" Name="ktor.exe" DiskId="1" Source="${toolPath}" KeyPath="yes" />
+                    </Component>
+                </Directory>
+            </Directory>
+        </StandardDirectory>
+
+        <ui:WixUI Id="WixUI_InstallDir" InstallDirectory="INSTALLDIR" />
+    </Package>
+</Wix>
+"@
+
+$wixProduct | out-file -filepath KtorProduct.wxs
+& $wixExe build -arch x64 -o $outPath -ext WixToolset.UI.wixext KtorProduct.wxs
+Remove-Item KtorProduct.wxs
