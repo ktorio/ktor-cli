a8ce0ec44bfbdb1ca0c0709009fc147dcbea5e1a
Aleksei Tirman <aleksei.tirman@jetbrains.com>
2024-09-05 11:41:01 +0300

    KTOR-7395 Fix downloaded JDK paths on macOS

--- internal/app/cli/jdk.go
+++ internal/app/cli/jdk.go
@@ -4,11 +4,9 @@ import (
 	"errors"
 	"fmt"
 	"github.com/ktorio/ktor-cli/internal/app"
-	"github.com/ktorio/ktor-cli/internal/app/config"
 	"github.com/ktorio/ktor-cli/internal/app/jdk"
 	"log"
 	"net/http"
-	"os"
 )
 
 func DownloadJdk(homeDir string, client *http.Client, logger *log.Logger, attempt int) (string, error) {
@@ -16,18 +14,8 @@ func DownloadJdk(homeDir string, client *http.Client, logger *log.Logger, attemp
 
 	if err != nil {
 		var e *app.Error
-		var pe *os.PathError
 
-		isAppError := errors.As(err, &e)
-		// JDK is already downloaded but the config doesn't reflect that
-		if isAppError && errors.As(e.Err, &pe) && errors.Is(pe.Err, os.ErrExist) {
-			jdkPath = pe.Path
-			config.SetValue("jdk", pe.Path)
-			_ = config.Commit()
-			return pe.Path, nil
-		}
-
-		if isAppError && e.Kind == app.JdkVerificationFailed {
+		if errors.As(err, &e) && e.Kind == app.JdkVerificationFailed {
 			fmt.Println("JDK Verification Failed. Trying again...")
 
 			if attempt >= 2 {
